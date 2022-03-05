79019dd02e741978a657507208a8aa595f737b7c
Aniket31-coder <aniketdewnani31@gmail.com>
2022-03-05 12:59:49 +0530

    Added Pages

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Row, Form, Col, Button } from 'react-bootstrap'
 
 function ContactUs() {
-  return (
-    <div>ContactUs</div>
-  )
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [age, setAge] = useState('');
+    const [password, setPassword] = useState('');
+    const [doubt, setdoubt] = useState('');
+    const [fileInput, setFileInput] = useState('');
+    const [isCounselor, setIsCounselor] = useState();
+
+    const handleNameChange = e => {
+        setName(e.target.value);
+    };
+
+    const handleEmailChange = e => {
+            setEmail(e.target.value);
+    };
+
+    const handleAgeChange = e => {
+        setAge(e.target.value);
+    };
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value);
+    };
+
+    const handledoubtChange = e => {
+        setdoubt(e.target.value);
+    };
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    return (
+        <div>
+        <Row className="w-100 mx-0 justify-content-center mt-4">
+            <Col xs={12}>
+                <h1 className='text-center' style={{fontFamily: "Rasa", color: "#FFE45"}}>Contact Us</h1>
+            </Col>
+        </Row>
+        <div className='contactUs-page'>
+            <Container align="center" style={{marginBottom: "20px"}}>
+                <div className="contact-form">
+                    <Form align="left" style={{paddingTop: "20px"}} onSubmit={handleSubmit}>
+                        
+                        <Form.Group controlId="name" className="fields">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" placeholder="Your Name" onChange={handleNameChange} required/>
+                        </Form.Group>
+
+                        <Form.Group controlId="email" className="fields">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Your Email-ID" onChange={handleEmailChange} required/>
+                        </Form.Group>
+
+                        <Form.Group controlId="age" className="fields">
+                            <Form.Label>Age</Form.Label>
+                            <Form.Control type="number" placeholder="Your Age" onChange={handleAgeChange} required />
+                        </Form.Group>
+
+                        <Form.Group controlId="doubt" className="fields">
+                            <Form.Label style={{marginTop: "1px" }}>Ask Doubt</Form.Label>
+                            <Form.Control type="textarea" placeholder="Ask Your Doubts" onChange={handledoubtChange} required/>
+                        </Form.Group>
+                        
+                        {/* <Form.Group controlId="formBasicCheckbox" align="center">
+                            <Form.Check type="checkbox" label="Agree to the terms and conditions." required/>
+                        </Form.Group> */}
+                        
+                        <div className="center">
+                        <Button variant="danger" type="submit" style={{width: "130px",background: "#FF6981", fontSize: "25px" ,height: "50px",marginTop: "1rem"}}>
+                            SUBMIT
+                        </Button>
+                        </div>
+                    </Form>
+                </div>
+            </Container>
+        </div>
+    </div>
+    )
 }
 
 export default ContactUs
--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 
 function OurTeam() {
   return (
-    <div>OurTeam</div>
+    <Container fluid className="px-0">
+        <Row className="mx-0 pt-4 pt-2" style={{backgroundColor: "rgb(109 66 113)"}}>
+            <Col xs={12}>
+                <h4 className='text-center fs-1 text-white' style={{fontFamily: "Rasa"}}>Our Team</h4>
+            </Col>
+        </Row>
+
+        <Row className='justify-content-center py-3 mx-0' style={{backgroundColor: "rgb(109 66 113)"}}>
+            <Col xs={12} md={4} className='pb-3 mb-md-0'>
+                <Card className='mx-2 mx-lg-5 text-center bg-transparent border-0'>
+                <Card.Img src='./assets/avatar-1.jpg' className="cardImg" alt='card-image' variant='top' style={{borderRadius: "50%"}} />
+                <Card.Body>
+                    <h4 className='mt-2' style={{color: "#FFF", paddingTop: "5px"}}>Mrs. Lorem Ipsum</h4>
+                </Card.Body>
+                </Card>
+            </Col>
+            <Col xs={12} md={4} className='pb-3 mb-md-0'>
+                <Card className='mx-2 mx-lg-5 text-center bg-transparent border-0'>
+                <Card.Img src='./assets/avatar-2.jpg' className="cardImg" alt='card-image' variant='top' style={{borderRadius: "50%"}} />
+                <Card.Body>
+                    <h4 className='mt-2' style={{color: "#FFF", paddingTop: "5px"}}>Mr. Lorem Ipsum</h4>
+                </Card.Body>
+                </Card>
+            </Col>
+            <Col xs={12} md={4} className='pb-3 mb-md-0'>
+                <Card className='mx-2 mx-lg-5 text-center bg-transparent border-0'>
+                <Card.Img src='./assets/avatar-3.jpg' className="cardImg" alt='card-image' variant='top' style={{borderRadius: "50%"}} />
+                <Card.Body>
+                    <h4 className='mt-2' style={{color: "#FFF", paddingTop: "5px"}}>Mr. Bruce Wayne</h4>
+                </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
